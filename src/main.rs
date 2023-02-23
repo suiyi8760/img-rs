27761bd5d1867791d7a19704afc605623be7d995
Johnny <suiyi8760@sina.com>
2023-02-24 01:18:41 +0800

    feat: add server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
+use axum::{extract::Path, handler::get, http::StatusCode, Router};
+use percent_encoding::percent_decode_str;
+use serde::Deserialize;
+use std::convert::TryInto;
+
 mod pb;
+
+use pb::*;
+
+// 参数使用 serde 做 Deserialize，axum 会自动识别并解析
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+// 目前我们就只把参数解析出来
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    Ok(format!("url: {}\n spec: {:#?}", url, spec))
+}
 
 fn main() {
     println!("Hello, world!");
